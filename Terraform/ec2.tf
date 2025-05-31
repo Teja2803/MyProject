f889b0b2d75ff8bb3d46f6f4f99b3add7d7e5661
Teja2803 <tejaswini055@gmail.com>
2025-05-31 11:05:39 +0530

    ec2

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -1,4 +1,5 @@
 module "ec2_instance" {
+  depends_on = [module.vpc]
   source  = "terraform-aws-modules/ec2-instance/aws"
 
   name = "Jenkins"
@@ -7,7 +8,7 @@ module "ec2_instance" {
   key_name               = "key"
   monitoring             = true
   vpc_security_group_ids = [module.vpc.default_security_group_id]
-  subnet_id              = module.vpc.public_subnets.subnet_id
+  subnet_id              = module.vpc.public_subnets[0]
 
   tags = {
     Terraform   = "true"
