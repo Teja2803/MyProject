9e99595e45e3c11ce8894908bb4065c64b24b1e1
Teja2803 <tejaswini055@gmail.com>
2025-05-31 11:11:55 +0530

    op

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -28,35 +28,6 @@ output "default_route_table_id" {
   value       = module.vpc.default_route_table_id
 }
 
-output "vpc_instance_tenancy" {
-  description = "Tenancy of instances spin up within VPC"
-  value       = module.vpc.vpc_instance_tenancy
-}
-
-output "vpc_enable_dns_support" {
-  description = "Whether or not the VPC has DNS support"
-  value       = module.vpc.vpc_enable_dns_support
-}
-
-output "vpc_enable_dns_hostnames" {
-  description = "Whether or not the VPC has DNS hostname support"
-  value       = module.vpc.vpc_enable_dns_hostnames
-}
-
-output "vpc_main_route_table_id" {
-  description = "The ID of the main route table associated with this VPC"
-  value       = module.vpc.vpc_main_route_table_id
-}
-
-output "vpc_ipv6_association_id" {
-  description = "The association ID for the IPv6 CIDR block"
-  value       = module.vpc.vpc_ipv6_association_id
-}
-
-output "vpc_ipv6_cidr_block" {
-  description = "The IPv6 CIDR block"
-  value       = module.vpc.vpc_ipv6_cidr_block
-}
 
 output "vpc_secondary_cidr_blocks" {
   description = "List of secondary CIDR blocks of the VPC"
@@ -73,42 +44,8 @@ output "private_subnets" {
   value       = module.vpc.private_subnets
 }
 
-output "private_subnet_arns" {
-  description = "List of ARNs of private subnets"
-  value       = module.vpc.private_subnet_arns
-}
-
-output "private_subnets_cidr_blocks" {
-  description = "List of cidr_blocks of private subnets"
-  value       = module.vpc.private_subnets_cidr_blocks
-}
-
-output "private_subnets_ipv6_cidr_blocks" {
-  description = "List of IPv6 cidr_blocks of private subnets in an IPv6 enabled VPC"
-  value       = module.vpc.private_subnets_ipv6_cidr_blocks
-}
 
 output "public_subnets" {
   description = "List of IDs of public subnets"
   value       = module.vpc.public_subnets
-}
-
-output "public_subnet_arns" {
-  description = "List of ARNs of public subnets"
-  value       = module.vpc.public_subnet_arns
-}
-
-output "public_subnets_cidr_blocks" {
-  description = "List of cidr_blocks of public subnets"
-  value       = module.vpc.public_subnets_cidr_blocks
-}
-
-output "public_subnets_ipv6_cidr_blocks" {
-  description = "List of IPv6 cidr_blocks of public subnets in an IPv6 enabled VPC"
-  value       = module.vpc.public_subnets_ipv6_cidr_blocks
-}
-
-output "outpost_subnets" {
-  description = "List of IDs of outpost subnets"
-  value       = module.vpc.outpost_subnets
 }
