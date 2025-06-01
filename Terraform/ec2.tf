d414fbd2554fba80007d695d2b90d3be00be5d9f
Teja2803 <tejaswini055@gmail.com>
2025-06-01 09:41:57 +0530

    added eks.yaml

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -6,6 +6,7 @@ module "ec2_instance" {
 
   instance_type          = "t2.medium"
   ami                    = "ami-0e35ddab05955cf57"
+  public_ip             = true
   iam_instance_profile    = "ec2"
   key_name               = "key"
   monitoring             = true
